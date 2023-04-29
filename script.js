e1c78ee548545ddd5bd1b74cae4b5dfd24ee2b4b
Ronald <ronaldreyjr061201@gmail.com>
2023-04-29 20:59:25 +0800

    removed comments that are not needed anymore

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-/**
- * functions to make
- * 
- * change the heart when favorite is pressed !
- * change the read when pressed
- * remove the div when x is pressed 
- */
-
 let hearts = document.querySelectorAll('.likes .hoveredHeart');
 hearts.forEach(elem => {
     elem.addEventListener('click',changeHeart);
